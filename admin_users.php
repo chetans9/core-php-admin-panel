4069770e1e7c5b6e399dde3bd0202a9c4e1a0d51
Felipe Abranches de Araujo Porto <42833892+peixespnsc@users.noreply.github.com>
2019-07-12 12:04:44 -0300

    Several changes
    Use of new classes on filters; absolute paths; more consistent variables names; better buttons (links and icons); changes on indenting and comments and so on.

--- admin_users.php
+++ admin_users.php
@@ -1,192 +1,197 @@
 <?php
 session_start();
-require_once './config/config.php';
-require_once 'includes/auth_validate.php';
+require_once BASE_PATH.'/config/config.php';
+require_once BASE_PATH.'/includes/auth_validate.php';
 
-//Only super admin is allowed to access this page
-if ($_SESSION['admin_type'] !== 'super') {
-    // show permission denied message
+// Users class
+require_once BASE_PATH.'/lib/Users/Users.php';
+$users = new Users();
+
+// Only super admin is allowed to access this page
+if ($_SESSION['admin_type'] !== 'super')
+{
+    // Show permission denied message
     header('HTTP/1.1 401 Unauthorized', true, 401);
-    
-    exit("401 Unauthorized");
+    exit('401 Unauthorized');
 }
-//Get data from query string
-$search_string = filter_input(INPUT_GET, 'search_string');
-$del_id = filter_input(INPUT_GET, 'del_id');
 
+// Get Input data from query string
+$search_string = filter_input(INPUT_GET, 'search_string');
 $filter_col = filter_input(INPUT_GET, 'filter_col');
 $order_by = filter_input(INPUT_GET, 'order_by');
-$page = filter_input(INPUT_GET, 'page');
+$del_id = filter_input(INPUT_GET, 'del_id');
+
+// Per page limit for pagination.
 $pagelimit = 20;
-if ($page == "") {
+
+// Get current page.
+$page = filter_input(INPUT_GET, 'page');
+if (!$page)
+{
     $page = 1;
 }
+
 // If filter types are not selected we show latest added data first
-if ($filter_col == "") {
-    $filter_col = "id";
+if (!$filter_col)
+{
+    $filter_col = 'id';
 }
-if ($order_by == "") {
-    $order_by = "desc";
+if (!$order_by)
+{
+    $order_by = 'Desc';
 }
 
 //Get DB instance. i.e instance of MYSQLiDB Library
 $db = getDbInstance();
 $select = array('id', 'user_name', 'admin_type');
 
-// If user searches 
-if ($search_string) {
+//Start building query according to input parameters.
+// If search string
+if ($search_string)
+{
     $db->where('user_name', '%' . $search_string . '%', 'like');
 }
 
-
-if ($order_by) {
+//If order by option selected
+if ($order_by)
+{
     $db->orderBy($filter_col, $order_by);
 }
 
+// Set pagination limit
 $db->pageLimit = $pagelimit;
-$result = $db->arraybuilder()->paginate("admin_accounts", $page, $select);
+
+// Get result of the query.
+$rows = $db->arraybuilder()->paginate('admin_accounts', $page, $select);
 $total_pages = $db->totalPages;
 
-
-// get columns for order filter
-foreach ($result as $value) {
-    foreach ($value as $col_name => $col_value) {
-        $filter_options[$col_name] = $col_name;
-    }
-    //execute only once
-    break;
-}
-
-
-include_once 'includes/header.php';
+include BASE_PATH.'/includes/header.php';
 ?>
-
+<!-- Main container -->
 <div id="page-wrapper">
-<div class="row">
-     <div class="col-lg-6">
+    <div class="row">
+        <div class="col-lg-6">
             <h1 class="page-header">Admin users</h1>
         </div>
-        <div class="col-lg-6" style="">
+        <div class="col-lg-6">
             <div class="page-action-links text-right">
-            <a href="add_admin.php"> <button class="btn btn-success">Add new</button></a>
+                <a href="add_admin.php" class="btn btn-success"><i class="glyphicon glyphicon-plus"></i> Add new</a>
             </div>
         </div>
-</div>
- <?php include('./includes/flash_messages.php') ?>
+    </div>
+    <?php include BASE_PATH.'/includes/flash_messages.php'; ?>
 
     <?php
-    if (isset($del_stat) && $del_stat == 1) {
+    if (isset($del_stat) && $del_stat == 1)
+    {
         echo '<div class="alert alert-info">Successfully deleted</div>';
     }
     ?>
     
-    <!--    Begin filter section-->
+    <!-- Filters -->
     <div class="well text-center filter-form">
         <form class="form form-inline" action="">
-            <label for="input_search" >Search</label>
-            <input type="text" class="form-control" id="input_search"  name="search_string" value="<?php echo htmlspecialchars($search_string, ENT_QUOTES, 'UTF-8'); ?>">
-            <label for ="input_order">Order By</label>
+            <label for="input_search">Search</label>
+            <input type="text" class="form-control" id="input_search" name="search_string" value="<?php echo htmlspecialchars($search_string, ENT_QUOTES, 'UTF-8'); ?>">
+            <label for="input_order">Order By</label>
             <select name="filter_col" class="form-control">
-
                 <?php
-                foreach ($filter_options as $option) {
-                    ($filter_col === $option) ? $selected = "selected" : $selected = "";
-                    echo ' <option value="' . $option . '" ' . $selected . '>' . $option . '</option>';
-                }
+                foreach ($users->setOrderingValues() as $opt_value => $opt_name):
+                    ($order_by === $opt_value) ? $selected = 'selected' : $selected = '';
+                    echo ' <option value="'.$opt_value.'" '.$selected.'>'.$opt_name.'</option>';
+                endforeach;
                 ?>
-
             </select>
-
             <select name="order_by" class="form-control" id="input_order">
-
                 <option value="Asc" <?php
                 if ($order_by == 'Asc') {
-                    echo "selected";
+                    echo 'selected';
                 }
                 ?> >Asc</option>
                 <option value="Desc" <?php
                 if ($order_by == 'Desc') {
-                    echo "selected";
+                    echo 'selected';
                 }
                 ?>>Desc</option>
             </select>
             <input type="submit" value="Go" class="btn btn-primary">
-
         </form>
     </div>
-    <!--   Filter section end-->
     <hr>
+    <!-- //Filters -->
+
+    <!-- Table -->
     <table class="table table-striped table-bordered table-condensed">
         <thead>
             <tr>
-                <th class="header">#</th>
-                <th>Name</th>
-                <th>Admin type</th>
-                <th>Actions</th>
+                <th width="5%">ID</th>
+                <th width="45%">Name</th>
+                <th width="40%">Admin type</th>
+                <th width="10%">Actions</th>
             </tr>
         </thead>
         <tbody>
-
-            <?php foreach ($result as $row) : ?>
-                
+            <?php foreach ($rows as $row): ?>
             <tr>
-                <td><?php echo $row['id'] ?></td>
-                <td><?php echo htmlspecialchars($row['user_name']) ?></td>
-                <td><?php echo htmlspecialchars($row['admin_type']) ?></td>
-
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo htmlspecialchars($row['user_name']); ?></td>
+                <td><?php echo htmlspecialchars($row['admin_type']); ?></td>
                 <td>
-                    <a href="edit_admin.php?admin_user_id=<?php echo $row['id']?>&operation=edit" class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
-
-                    <a href=""  class="btn btn-danger delete_btn" data-toggle="modal" data-target="#confirm-delete-<?php echo $row['id'] ?>" style="margin-right: 8px;"><span class="glyphicon glyphicon-trash"></span>
-                    
+                    <a href="edit_admin.php?admin_user_id=<?php echo $row['id']; ?>&operation=edit" class="btn btn-primary"><i class="glyphicon glyphicon-edit"></i></a>
+                    <a href="#" class="btn btn-danger delete_btn" data-toggle="modal" data-target="#confirm-delete-<?php echo $row['id']; ?>"><i class="glyphicon glyphicon-trash"></i></a>
                 </td>
             </tr>
-                <!-- Delete Confirmation Modal-->
-                     <div class="modal fade" id="confirm-delete-<?php echo $row['id'] ?>" role="dialog">
-                        <div class="modal-dialog">
-                          <form action="delete_user.php" method="POST">
-                          <!-- Modal content-->
-                              <div class="modal-content">
-                                <div class="modal-header">
-                                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                  <h4 class="modal-title">Confirm</h4>
-                                </div>
-                                <div class="modal-body">
-                                    <input type="hidden" name="del_id" id = "del_id" value="<?php echo $row['id'] ?>">
-                                    <p>Are you sure you want to delete this user?</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="submit" class="btn btn-default pull-left">Yes</button>
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-                                </div>
-                              </div>
-                          </form>
-                          
+            <!-- Delete Confirmation Modal -->
+            <div class="modal fade" id="confirm-delete-<?php echo $row['id']; ?>" role="dialog">
+                <div class="modal-dialog">
+                    <form action="delete_user.php" method="POST">
+                        <!-- Modal content -->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Confirm</h4>
+                            </div>
+                            <div class="modal-body">
+                                <input type="hidden" name="del_id" id="del_id" value="<?php echo $row['id']; ?>">
+                                <p>Are you sure you want to delete this row?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-default pull-left">Yes</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                            </div>
                         </div>
-                    </div>
-            <?php endforeach; ?>   
+                    </form>
+                </div>
+            </div>
+            <!-- //Delete Confirmation Modal -->
+            <?php endforeach; ?>
         </tbody>
     </table>
-    <!--    Pagination links-->
-    <div class="text-center">
+    <!-- //Table -->
 
+    <!-- Pagination -->
+    <div class="text-center">
         <?php
         if (!empty($_GET)) {
-            //we must unset $_GET[page] if built by http_build_query function
+            // We must unset $_GET[page] if previously built by http_build_query function
             unset($_GET['page']);
+            // To keep the query sting parameters intact while navigating to next/prev page,
             $http_query = "?" . http_build_query($_GET);
         } else {
             $http_query = "?";
         }
+        // Show pagination links
         if ($total_pages > 1) {
             echo '<ul class="pagination text-center">';
             for ($i = 1; $i <= $total_pages; $i++) {
-                ($page == $i) ? $li_class = ' class="active"' : $li_class = "";
-                echo '<li' . $li_class . '><a href="index.php' . $http_query . '&page=' . $i . '">' . $i . '</a></li>';
+                ($page == $i) ? $li_class = ' class="active"' : $li_class = '';
+                echo '<li' . $li_class . '><a href="admin_users.php' . $http_query . '&page=' . $i . '">' . $i . '</a></li>';
             }
-            echo '</ul></div>';
+            echo '</ul>';
         }
         ?>
     </div>
+    <!-- //Pagination -->
 </div>
-<?php include_once 'includes/footer.php'; ?>
+<!-- //Main container -->
+<?php include BASE_PATH.'/includes/footer.php'; ?>
--- customers.php
+++ customers.php
@@ -1,29 +1,35 @@
 <?php
 session_start();
-require_once './config/config.php';
-require_once 'includes/auth_validate.php';
+require_once BASE_PATH.'/config/config.php';
+require_once BASE_PATH.'/includes/auth_validate.php';
 
-//Get Input data from query string
+// Costumers class
+require_once BASE_PATH.'/lib/Costumers/Costumers.php';
+$costumers = new Costumers();
+
+// Get Input data from query string
 $search_string = filter_input(INPUT_GET, 'search_string');
 $filter_col = filter_input(INPUT_GET, 'filter_col');
 $order_by = filter_input(INPUT_GET, 'order_by');
 
-//Get current page.
-$page = filter_input(INPUT_GET, 'page');
-
-//Per page limit for pagination.
+// Per page limit for pagination.
 $pagelimit = 20;
 
-if (!$page) {
+// Get current page.
+$page = filter_input(INPUT_GET, 'page');
+if (!$page)
+{
     $page = 1;
 }
 
-// If filter types are not selected we show latest created data first
-if (!$filter_col) {
-    $filter_col = "created_at";
+// If filter types are not selected we show latest added data first
+if (!$filter_col)
+{
+    $filter_col = 'id';
 }
-if (!$order_by) {
-    $order_by = "Desc";
+if (!$order_by)
+{
+    $order_by = 'Desc';
 }
 
 //Get DB instance. i.e instance of MYSQLiDB Library
@@ -32,7 +38,7 @@ $select = array('id', 'f_name', 'l_name', 'gender', 'phone','created_at','update
 
 //Start building query according to input parameters.
 // If search string
-if ($search_string) 
+if ($search_string)
 {
     $db->where('f_name', '%' . $search_string . '%', 'like');
     $db->orwhere('l_name', '%' . $search_string . '%', 'like');
@@ -44,161 +50,135 @@ if ($order_by)
     $db->orderBy($filter_col, $order_by);
 }
 
-//Set pagination limit
+// Set pagination limit
 $db->pageLimit = $pagelimit;
 
-//Get result of the query.
-$customers = $db->arraybuilder()->paginate("customers", $page, $select);
+// Get result of the query.
+$rows = $db->arraybuilder()->paginate('customers', $page, $select);
 $total_pages = $db->totalPages;
 
-// get columns for order filter
-foreach ($customers as $value) {
-    foreach ($value as $col_name => $col_value) {
-        $filter_options[$col_name] = $col_name;
-    }
-    //execute only once
-    break;
-}
-include_once 'includes/header.php';
+include BASE_PATH.'/includes/header.php';
 ?>
-
-<!--Main container start-->
+<!-- Main container -->
 <div id="page-wrapper">
     <div class="row">
-
         <div class="col-lg-6">
             <h1 class="page-header">Customers</h1>
         </div>
-        <div class="col-lg-6" style="">
+        <div class="col-lg-6">
             <div class="page-action-links text-right">
-	            <a href="add_customer.php?operation=create">
-	            	<button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> Add new </button>
-	            </a>
+                <a href="add_customer.php?operation=create" class="btn btn-success"><i class="glyphicon glyphicon-plus"></i> Add new</a>
             </div>
         </div>
     </div>
-        <?php include('./includes/flash_messages.php') ?>
-    <!--    Begin filter section-->
+    <?php include BASE_PATH.'/includes/flash_messages.php'; ?>
+
+    <!-- Filters -->
     <div class="well text-center filter-form">
         <form class="form form-inline" action="">
             <label for="input_search">Search</label>
             <input type="text" class="form-control" id="input_search" name="search_string" value="<?php echo htmlspecialchars($search_string, ENT_QUOTES, 'UTF-8'); ?>">
-            <label for ="input_order">Order By</label>
+            <label for="input_order">Order By</label>
             <select name="filter_col" class="form-control">
-
                 <?php
-                foreach ($filter_options as $option) {
-                    ($filter_col === $option) ? $selected = "selected" : $selected = "";
-                    echo ' <option value="' . $option . '" ' . $selected . '>' . $option . '</option>';
-                }
+                foreach ($costumers->setOrderingValues() as $opt_value => $opt_name):
+                    ($order_by === $opt_value) ? $selected = 'selected' : $selected = '';
+                    echo ' <option value="'.$opt_value.'" '.$selected.'>'.$opt_name.'</option>';
+                endforeach;
                 ?>
-
             </select>
-
             <select name="order_by" class="form-control" id="input_order">
-
                 <option value="Asc" <?php
                 if ($order_by == 'Asc') {
-                    echo "selected";
+                    echo 'selected';
                 }
                 ?> >Asc</option>
                 <option value="Desc" <?php
                 if ($order_by == 'Desc') {
-                    echo "selected";
+                    echo 'selected';
                 }
                 ?>>Desc</option>
             </select>
             <input type="submit" value="Go" class="btn btn-primary">
-
         </form>
     </div>
-<!--   Filter section end-->
-
     <hr>
+    <!-- //Filters -->
 
-
+    <!-- Table -->
     <table class="table table-striped table-bordered table-condensed">
         <thead>
             <tr>
-                <th class="header">#</th>
-                <th>Name</th>
-                <th>Gender</th>
-                <th>phone</th>
-                <th>Actions</th>
+                <th width="5%">ID</th>
+                <th width="45%">Name</th>
+                <th width="20%">Gender</th>
+                <th width="20%">Phone</th>
+                <th width="10%">Actions</th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($customers as $row) : ?>
-                <tr>
-	                <td><?php echo $row['id'] ?></td>
-	                <td><?php echo htmlspecialchars($row['f_name']." ".$row['l_name']); ?></td>
-	                <td><?php echo htmlspecialchars($row['gender']) ?></td>
-	                <td><?php echo htmlspecialchars($row['phone']) ?> </td>
-	                <td>
-					<a href="edit_customer.php?customer_id=<?php echo $row['id'] ?>&operation=edit" class="btn btn-primary" style="margin-right: 8px;"><span class="glyphicon glyphicon-edit"></span>
-
-					<a href=""  class="btn btn-danger delete_btn" data-toggle="modal" data-target="#confirm-delete-<?php echo $row['id'] ?>" style="margin-right: 8px;"><span class="glyphicon glyphicon-trash"></span></td>
-				</tr>
-
-						<!-- Delete Confirmation Modal-->
-					 <div class="modal fade" id="confirm-delete-<?php echo $row['id'] ?>" role="dialog">
-					    <div class="modal-dialog">
-					      <form action="delete_customer.php" method="POST">
-					      <!-- Modal content-->
-						      <div class="modal-content">
-						        <div class="modal-header">
-						          <button type="button" class="close" data-dismiss="modal">&times;</button>
-						          <h4 class="modal-title">Confirm</h4>
-						        </div>
-						        <div class="modal-body">
-						      
-						        		<input type="hidden" name="del_id" id = "del_id" value="<?php echo $row['id'] ?>">
-						        	
-						          <p>Are you sure you want to delete this customer?</p>
-						        </div>
-						        <div class="modal-footer">
-						        	<button type="submit" class="btn btn-default pull-left">Yes</button>
-						         	<button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-						        </div>
-						      </div>
-					      </form>
-					      
-					    </div>
-  					</div>
-            <?php endforeach; ?>      
+            <?php foreach ($rows as $row): ?>
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo htmlspecialchars($row['f_name'].' '.$row['l_name']); ?></td>
+                <td><?php echo htmlspecialchars($row['gender']); ?></td>
+                <td><?php echo htmlspecialchars($row['phone']); ?></td>
+                <td>
+                    <a href="edit_customer.php?customer_id=<?php echo $row['id']; ?>&operation=edit" class="btn btn-primary"><i class="glyphicon glyphicon-edit"></i></a>
+                    <a href="#" class="btn btn-danger delete_btn" data-toggle="modal" data-target="#confirm-delete-<?php echo $row['id']; ?>"><i class="glyphicon glyphicon-trash"></i></a>
+                </td>
+            </tr>
+            <!-- Delete Confirmation Modal -->
+            <div class="modal fade" id="confirm-delete-<?php echo $row['id']; ?>" role="dialog">
+                <div class="modal-dialog">
+                    <form action="delete_customer.php" method="POST">
+                        <!-- Modal content -->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Confirm</h4>
+                            </div>
+                            <div class="modal-body">
+                                <input type="hidden" name="del_id" id="del_id" value="<?php echo $row['id']; ?>">
+                                <p>Are you sure you want to delete this row?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="submit" class="btn btn-default pull-left">Yes</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- //Delete Confirmation Modal -->
+            <?php endforeach; ?>
         </tbody>
     </table>
+    <!-- //Table -->
 
-
-   
-<!--    Pagination links-->
+    <!-- Pagination -->
     <div class="text-center">
-
         <?php
         if (!empty($_GET)) {
-            //we must unset $_GET[page] if previously built by http_build_query function
+            // We must unset $_GET[page] if previously built by http_build_query function
             unset($_GET['page']);
-            //to keep the query sting parameters intact while navigating to next/prev page,
+            // To keep the query sting parameters intact while navigating to next/prev page,
             $http_query = "?" . http_build_query($_GET);
         } else {
             $http_query = "?";
         }
-        //Show pagination links
+        // Show pagination links
         if ($total_pages > 1) {
             echo '<ul class="pagination text-center">';
             for ($i = 1; $i <= $total_pages; $i++) {
-                ($page == $i) ? $li_class = ' class="active"' : $li_class = "";
+                ($page == $i) ? $li_class = ' class="active"' : $li_class = '';
                 echo '<li' . $li_class . '><a href="customers.php' . $http_query . '&page=' . $i . '">' . $i . '</a></li>';
             }
-            echo '</ul></div>';
+            echo '</ul>';
         }
         ?>
     </div>
-    <!--    Pagination links end-->
-
+    <!-- //Pagination -->
 </div>
-<!--Main container end-->
-
-
-<?php include_once './includes/footer.php'; ?>
-
+<!-- //Main container -->
+<?php include BASE_PATH.'/includes/footer.php'; ?>
