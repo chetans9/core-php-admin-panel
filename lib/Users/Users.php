4e9c0bf6ff00e143440aee503d43d54e47ec2853
Felipe Abranches de Araujo Porto <42833892+peixespnsc@users.noreply.github.com>
2019-07-12 11:46:32 -0300

    Initializing classes
    Initializing classes to split modeling from viewing

---
+++ lib/Users/Users.php
@@ -0,0 +1,32 @@
+<?php
+class Users
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     *
+     */
+    public function __destruct()
+    {
+    }
+    
+    /**
+     * Set friendly columns\' names to order tables\' entries
+     */
+    public function setOrderingValues()
+    {
+        $ordering = [
+            'id' => 'ID',
+            'user_name' => 'User Name',
+            'admin_type' => 'Admin Type'
+        ];
+
+        return $ordering;
+    }
+}
+?>
