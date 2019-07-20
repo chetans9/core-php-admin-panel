2c3affbac0978187f7e371ba66d68ca25bc7f51d
chetans9 <chetanshenai9@gmail.com>
2019-07-20 17:18:35 +0530

    Reduced pagination links

--- customers.php
+++ customers.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 require_once 'config/config.php';
-require_once BASE_PATH.'/includes/auth_validate.php';
+require_once BASE_PATH . '/includes/auth_validate.php';
 
 // Costumers class
-require_once BASE_PATH.'/lib/Costumers/Costumers.php';
+require_once BASE_PATH . '/lib/Costumers/Costumers.php';
 $costumers = new Costumers();
 
 // Get Input data from query string
@@ -13,41 +13,36 @@ $filter_col = filter_input(INPUT_GET, 'filter_col');
 $order_by = filter_input(INPUT_GET, 'order_by');
 
 // Per page limit for pagination.
-$pagelimit = 20;
+$pagelimit = 15;
 
 // Get current page.
 $page = filter_input(INPUT_GET, 'page');
-if (!$page)
-{
-    $page = 1;
+if (!$page) {
+	$page = 1;
 }
 
 // If filter types are not selected we show latest added data first
-if (!$filter_col)
-{
-    $filter_col = 'id';
+if (!$filter_col) {
+	$filter_col = 'id';
 }
-if (!$order_by)
-{
-    $order_by = 'Desc';
+if (!$order_by) {
+	$order_by = 'Desc';
 }
 
 //Get DB instance. i.e instance of MYSQLiDB Library
 $db = getDbInstance();
-$select = array('id', 'f_name', 'l_name', 'gender', 'phone','created_at','updated_at');
+$select = array('id', 'f_name', 'l_name', 'gender', 'phone', 'created_at', 'updated_at');
 
 //Start building query according to input parameters.
 // If search string
-if ($search_string)
-{
-    $db->where('f_name', '%' . $search_string . '%', 'like');
-    $db->orwhere('l_name', '%' . $search_string . '%', 'like');
+if ($search_string) {
+	$db->where('f_name', '%' . $search_string . '%', 'like');
+	$db->orwhere('l_name', '%' . $search_string . '%', 'like');
 }
 
 //If order by option selected
-if ($order_by)
-{
-    $db->orderBy($filter_col, $order_by);
+if ($order_by) {
+	$db->orderBy($filter_col, $order_by);
 }
 
 // Set pagination limit
@@ -57,7 +52,7 @@ $db->pageLimit = $pagelimit;
 $rows = $db->arraybuilder()->paginate('customers', $page, $select);
 $total_pages = $db->totalPages;
 
-include BASE_PATH.'/includes/header.php';
+include BASE_PATH . '/includes/header.php';
 ?>
 <!-- Main container -->
 <div id="page-wrapper">
@@ -71,7 +66,7 @@ include BASE_PATH.'/includes/header.php';
             </div>
         </div>
     </div>
-    <?php include BASE_PATH.'/includes/flash_messages.php'; ?>
+    <?php include BASE_PATH . '/includes/flash_messages.php';?>
 
     <!-- Filters -->
     <div class="well text-center filter-form">
@@ -81,23 +76,23 @@ include BASE_PATH.'/includes/header.php';
             <label for="input_order">Order By</label>
             <select name="filter_col" class="form-control">
                 <?php
-                foreach ($costumers->setOrderingValues() as $opt_value => $opt_name):
-                    ($order_by === $opt_value) ? $selected = 'selected' : $selected = '';
-                    echo ' <option value="'.$opt_value.'" '.$selected.'>'.$opt_name.'</option>';
-                endforeach;
-                ?>
+foreach ($costumers->setOrderingValues() as $opt_value => $opt_name):
+	($order_by === $opt_value) ? $selected = 'selected' : $selected = '';
+	echo ' <option value="' . $opt_value . '" ' . $selected . '>' . $opt_name . '</option>';
+endforeach;
+?>
             </select>
             <select name="order_by" class="form-control" id="input_order">
                 <option value="Asc" <?php
-                if ($order_by == 'Asc') {
-                    echo 'selected';
-                }
-                ?> >Asc</option>
+if ($order_by == 'Asc') {
+	echo 'selected';
+}
+?> >Asc</option>
                 <option value="Desc" <?php
-                if ($order_by == 'Desc') {
-                    echo 'selected';
-                }
-                ?>>Desc</option>
+if ($order_by == 'Desc') {
+	echo 'selected';
+}
+?>>Desc</option>
             </select>
             <input type="submit" value="Go" class="btn btn-primary">
         </form>
@@ -120,7 +115,7 @@ include BASE_PATH.'/includes/header.php';
             <?php foreach ($rows as $row): ?>
             <tr>
                 <td><?php echo $row['id']; ?></td>
-                <td><?php echo htmlspecialchars($row['f_name'].' '.$row['l_name']); ?></td>
+                <td><?php echo htmlspecialchars($row['f_name'] . ' ' . $row['l_name']); ?></td>
                 <td><?php echo htmlspecialchars($row['gender']); ?></td>
                 <td><?php echo htmlspecialchars($row['phone']); ?></td>
                 <td>
@@ -151,34 +146,16 @@ include BASE_PATH.'/includes/header.php';
                 </div>
             </div>
             <!-- //Delete Confirmation Modal -->
-            <?php endforeach; ?>
+            <?php endforeach;?>
         </tbody>
     </table>
     <!-- //Table -->
 
     <!-- Pagination -->
     <div class="text-center">
-        <?php
-        if (!empty($_GET)) {
-            // We must unset $_GET[page] if previously built by http_build_query function
-            unset($_GET['page']);
-            // To keep the query sting parameters intact while navigating to next/prev page,
-            $http_query = "?" . http_build_query($_GET);
-        } else {
-            $http_query = "?";
-        }
-        // Show pagination links
-        if ($total_pages > 1) {
-            echo '<ul class="pagination text-center">';
-            for ($i = 1; $i <= $total_pages; $i++) {
-                ($page == $i) ? $li_class = ' class="active"' : $li_class = '';
-                echo '<li' . $li_class . '><a href="customers.php' . $http_query . '&page=' . $i . '">' . $i . '</a></li>';
-            }
-            echo '</ul>';
-        }
-        ?>
+    <?php echo paginationLinks($page, $total_pages, 'customers.php'); ?>
     </div>
     <!-- //Pagination -->
 </div>
 <!-- //Main container -->
-<?php include BASE_PATH.'/includes/footer.php'; ?>
+<?php include BASE_PATH . '/includes/footer.php';?>
