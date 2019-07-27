e1456055fa0c52d32186eb7563d49260102719f5
Felipe Abranches de Araujo Porto <42833892+peixespnsc@users.noreply.github.com>
2019-07-27 16:03:14 -0300

    Fix of the field "passwd" to "password" and others
    -Fix of the field "passwd" to "password" (to reflect DB changes)
    -401 code transfer to Users' class
    -Trying to achieve better similarity between "add_admin" and "edit_admin" codes

--- add_admin.php
+++ add_admin.php
@@ -1,65 +1,56 @@
 <?php
 session_start();
-require_once './config/config.php';
-require_once 'includes/auth_validate.php';
+require_once 'config/config.php';
+require_once BASE_PATH . '/includes/auth_validate.php';
 
-//Only super admin is allowed to access this page
-if ($_SESSION['admin_type'] !== 'super') {
-    // show permission denied message
-    echo 'Permission Denied';
-    exit();
-}
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
-	$data_to_store = filter_input_array(INPUT_POST);
-    $db = getDbInstance();
-    //Check whether the user name already exists ; 
-    $db->where('user_name',$data_to_store['user_name']);
-    $db->get('admin_accounts');
-    
-    if($db->count >=1){
-        $_SESSION['failure'] = "User name already exists";
-        header('location: add_admin.php');
-        exit();
-    }
-
-    //Encrypt password
-    $data_to_store['passwd'] = password_hash($data_to_store['passwd'],PASSWORD_DEFAULT);
-    //reset db instance
-    $db = getDbInstance();
-    $last_id = $db->insert ('admin_accounts', $data_to_store);
-    if($last_id)
-    {
-
-    	$_SESSION['success'] = "Admin user added successfully!";
-    	header('location: admin_users.php');
-    	exit();
-    }  
-    
-}
+// Users class
+require_once BASE_PATH . '/lib/Users/Users.php';
+$users = new Users();
 
 $edit = false;
 
+// Serve POST request
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	// Sanitize input post if we want
+	$data_to_db = filter_input_array(INPUT_POST);
 
-require_once 'includes/header.php';
+	// Check whether the user name already exists
+	$db = getDbInstance();
+	$db->where('user_name', $data_to_db['user_name']);
+	$db->get('admin_accounts');
+
+	if ($db->count >= 1)
+	{
+		$_SESSION['failure'] = 'Username already exists';
+		header('location: add_admin.php');
+		exit;
+	}
+
+	// Encrypting the password
+	$data_to_db['password'] = password_hash($data_to_db['password'], PASSWORD_DEFAULT);
+	// Reset db instance
+	$db = getDbInstance();
+	$last_id = $db->insert('admin_accounts', $data_to_db);
+
+	if ($last_id)
+	{
+		$_SESSION['success'] = 'Admin user added successfully';
+		header('location: admin_users.php');
+		exit;
+	}
+}
 ?>
+<?php include BASE_PATH . '/includes/header.php'; ?>
 <div id="page-wrapper">
 	<div class="row">
 		<div class="col-lg-12">
-			<h2 class="page-header">Add User</h2>
+			<h2 class="page-header"><?php echo (!$edit) ? 'Add' : 'Update'; ?> User</h2>
 		</div>
 	</div>
-	 <?php 
-    include_once('includes/flash_messages.php');
-    ?>
-	<form class="well form-horizontal" action=" " method="post"  id="contact_form" enctype="multipart/form-data">
-		<?php include_once './forms/admin_users_form.php'; ?>
+	<?php include BASE_PATH . '/includes/flash_messages.php'; ?>
+	<form class="well form-horizontal" action="" method="post" id="contact_form" enctype="multipart/form-data">
+		<?php include BASE_PATH . '/forms/admin_users_form.php'; ?>
 	</form>
 </div>
-
-
-
-
-<?php include_once 'includes/footer.php'; ?>
+<?php include BASE_PATH . '/includes/footer.php'; ?>
--- edit_admin.php
+++ edit_admin.php
@@ -1,36 +1,35 @@
 <?php
 session_start();
-require_once './config/config.php';
-require_once 'includes/auth_validate.php';
+require_once 'config/config.php';
+require_once BASE_PATH . '/includes/auth_validate.php';
 
-//User ID for which we are performing operation
+// Users class
+require_once BASE_PATH . '/lib/Users/Users.php';
+$users = new Users();
+
+// User ID for which we are performing operation
 $admin_user_id = filter_input(INPUT_GET, 'admin_user_id');
 $operation = filter_input(INPUT_GET, 'operation', FILTER_SANITIZE_STRING);
 ($operation == 'edit') ? $edit = true : $edit = false;
-//Serve POST request.
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	// If non-super user accesses this script via url. Stop the exexution
-	if ($_SESSION['admin_type'] !== 'super') {
-		// show permission denied message
-		echo 'Permission Denied';
-		exit();
-	}
 
+// Serve POST request
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
 	// Sanitize input post if we want
-	$data_to_update = filter_input_array(INPUT_POST);
-	//Check whether the user name already exists ;
+	$data_to_db = filter_input_array(INPUT_POST);
+
+	// Check whether the user name already exists
 	$db = getDbInstance();
-	$db->where('user_name', $data_to_update['user_name']);
+	$db->where('user_name', $data_to_db['user_name']);
 	$db->where('id', $admin_user_id, '!=');
-	//print_r($data_to_update['user_name']);die();
+	//print_r($data_to_db['user_name']);die();
 	$row = $db->getOne('admin_accounts');
-	//print_r($data_to_update['user_name']);
+	//print_r($data_to_db['user_name']);
 	//print_r($row); die();
 
-	if (!empty($row['user_name'])) {
-
-		$_SESSION['failure'] = "User name already exists";
-
+	if (!empty($row['user_name']))
+	{
+		$_SESSION['failure'] = 'Username already exists';
 		$query_string = http_build_query(array(
 			'admin_user_id' => $admin_user_id,
 			'operation' => $operation,
@@ -40,50 +39,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	}
 
 	$admin_user_id = filter_input(INPUT_GET, 'admin_user_id', FILTER_VALIDATE_INT);
-	//Encrypting the password
-	$data_to_update['passwd'] = password_hash($data_to_update['passwd'], PASSWORD_DEFAULT);
-
+	// Encrypting the password
+	$data_to_db['password'] = password_hash($data_to_db['password'], PASSWORD_DEFAULT);
+	// Reset db instance
 	$db = getDbInstance();
 	$db->where('id', $admin_user_id);
-	$stat = $db->update('admin_accounts', $data_to_update);
+	$stat = $db->update('admin_accounts', $data_to_db);
 
-	if ($stat) {
-		$_SESSION['success'] = "Admin user has been updated successfully";
+	if ($stat)
+	{
+		$_SESSION['success'] = 'Admin user has been updated successfully';
 	} else {
-		$_SESSION['failure'] = "Failed to update Admin user : " . $db->getLastError();
+		$_SESSION['failure'] = 'Failed to update Admin user: ' . $db->getLastError();
 	}
 
 	header('location: admin_users.php');
 	exit;
-
 }
 
-//Select where clause
+// Select where clause
 $db = getDbInstance();
 $db->where('id', $admin_user_id);
 
 $admin_account = $db->getOne("admin_accounts");
 
 // Set values to $row
-
-// import header
-require_once 'includes/header.php';
 ?>
+<?php include BASE_PATH . '/includes/header.php'; ?>
 <div id="page-wrapper">
-
-    <div class="row">
-     <div class="col-lg-12">
-            <h2 class="page-header">Update User</h2>
-        </div>
-
-    </div>
-    <?php include_once 'includes/flash_messages.php';?>
-    <form class="well form-horizontal" action="" method="post"  id="contact_form" enctype="multipart/form-data">
-        <?php include_once './forms/admin_users_form.php';?>
-    </form>
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="page-header"><?php echo (!$edit) ? 'Add' : 'Update'; ?> User</h2>
+		</div>
+	</div>
+	<?php include BASE_PATH . '/includes/flash_messages.php'; ?>
+	<form class="well form-horizontal" action="" method="post" id="contact_form" enctype="multipart/form-data">
+		<?php include BASE_PATH . '/forms/admin_users_form.php'; ?>
+	</form>
 </div>
-
-
-
-
-<?php include_once 'includes/footer.php';?>
+<?php include BASE_PATH . '/includes/footer.php'; ?>
